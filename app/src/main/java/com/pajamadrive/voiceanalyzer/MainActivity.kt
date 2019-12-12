a83d0f7392ad820f01fe5ef2e1300b210b14543d
PajamaDrive <pajamadrive49@gmail.com>
2019-12-12 21:24:16 +0900

    corresponding to swipe

--- app/src/main/java/com/pajamadrive/voiceanalyzer/MainActivity.kt
+++ app/src/main/java/com/pajamadrive/voiceanalyzer/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.pajamadrive.voiceanalyzer
 
-import android.Manifest
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.SurfaceView
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 import android.Manifest.permission.*
@@ -18,13 +15,18 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import android.util.Log
+import android.view.SurfaceView
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.viewpager.widget.ViewPager
 import java.lang.Math.pow
 import kotlin.math.log10
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentCheckListener, Runnable {
     private var record: Record? = null
     private var isRecording = false
     private var visualizer: VisualizerSurfaceView? = null
@@ -32,6 +34,12 @@ class MainActivity : AppCompatActivity() {
     private var permissionCheck: AccessPermissionCheck? = null
     private var file: WaveFile? = null
     private var storageCheck: ExternalStorageCheck? = null
+    private var viewPager: ViewPager? = null
+    private var sizeView: TextView? = null
+    private var vs: VisualizeSurfaceFragment? = null
+    private var df: DisplayFileFragment? = null
+    private var fc: FragmentCheck? = null
+    private var thread: Thread? = null
     private val OFFSET = 0
     private val PERMISSION_REQUEST_CODE = 1
     private val needPermissions = arrayOf(RECORD_AUDIO, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE)
@@ -43,25 +51,43 @@ class MainActivity : AppCompatActivity() {
     private val externalDirectoryPath: String
         get(){
             val dirPath = storageCheck?.getExternalStoragePath()
-            if(dirPath != null) {
+            if(dirPath != null)
                 if (dirPath != "")
                     return dirPath
-            }
             return getExternalFilesDir(null)!!.path
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewPager = findViewById(R.id.pageView)
+        vs = VisualizeSurfaceFragment()
+        df = DisplayFileFragment()
+        fc = FragmentCheck(arrayOf(vs!!, df!!))
+        fc?.setListener(this)
+        viewPager?.setAdapter(FragmetnPagerAdapter(supportFragmentManager, vs!!, df!!))
+        thread = Thread(this)
+        thread?.start()
+    }
 
-        val surface = findViewById(R.id.visualizer) as SurfaceView
+    override fun run() {
+        while(thread != null) {
+            fc?.checkFragment()
+        }
+    }
+
+    //fragmentの準備ができたら各種準備
+    override fun createListener(){
+        thread = null
+        val surface = vs?.getSurface()
         file = WaveFile()
         storageCheck = ExternalStorageCheck(this)
-        visualizer = VisualizerSurfaceView(this, surface)
         permissionCheck = AccessPermissionCheck()
         permissionCheck?.setPermissionExplain(needPermissions, PERMISSION_REQUEST_CODE,
             arrayOf("このアプリは録音を行うのでマイクの許可が必要です．", "このアプリは録音した音声を保存するためにストレージ書き込みの許可が必要です．", "このアプリは音声を再生するためにストレージ読み込みの許可が必要です．"))
-        button = findViewById(R.id.button_record) as Button
+        visualizer = VisualizerSurfaceView(this, surface!!)
+        button = vs?.getRecordButton()
+        sizeView = vs?.getText()
         button?.setOnClickListener{
             if(isRecording)
                 stopRecord()
@@ -109,7 +135,6 @@ class MainActivity : AppCompatActivity() {
             , {->}, packageName, requestCode, permissions, grantResults)
     }
 
-
     inner class Record : AsyncTask<Void, DoubleArray, Void>() {
         private val samplingRate = 44100
         //1回のFFTで使用する標本数
@@ -123,10 +148,7 @@ class MainActivity : AppCompatActivity() {
         private val INTERVAL = samplingRate / 300
         private val chCount = AudioFormat.CHANNEL_IN_MONO
         private val bitPerSample = AudioFormat.ENCODING_PCM_16BIT
-        private val minBufferSize = AudioRecord.getMinBufferSize(
-            samplingRate,
-            chCount,
-            bitPerSample) * 2
+        private val minBufferSize = AudioRecord.getMinBufferSize(samplingRate, chCount, bitPerSample) * 2
         private val sec = 2
         private var buffer = ShortArray(0)
         private var audioRecord: AudioRecord? = null
@@ -134,12 +156,7 @@ class MainActivity : AppCompatActivity() {
         init{
             visualizer?.initializeBuffer(samplingRate * sec / INTERVAL)
             buffer = ShortArray(minBufferSize)
-            audioRecord = AudioRecord(
-                MediaRecorder.AudioSource.MIC,
-                samplingRate,
-                chCount,
-                bitPerSample,
-                minBufferSize)
+            audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC, samplingRate, chCount, bitPerSample, minBufferSize)
             file?.createFile(externalDirectoryPath, fileName, if(chCount == AudioFormat.CHANNEL_IN_MONO) 1 else 2
                 , samplingRate, if(bitPerSample == AudioFormat.ENCODING_PCM_16BIT) 16 else 8)
             visualizer?.startDrawSurfaceThreed()
@@ -153,12 +170,10 @@ class MainActivity : AppCompatActivity() {
                     //byteArrayとかを使用するとリトルエンディアンのデータ(raw形式)が返ってくるっぽい
                     //shortArrayだと普通にビッグエンディアンで格納される
                     val readSize = audioRecord?.read(buffer, OFFSET, buffer.size) ?: -1
-                    if(readSize < 0){
+                    if(readSize < 0)
                         break
-                    }
-                    if(readSize == 0){
+                    if(readSize == 0)
                         continue
-                    }
                     file?.addBigEndianData(buffer)
 
                     val bigEndianDoubleBuffer = buffer.map{it.toDouble()}.toDoubleArray().copyOfRange(0, fftSize)
@@ -170,8 +185,8 @@ class MainActivity : AppCompatActivity() {
                     val maxDB = fftData.max()
                     val maxIndex = fftData.indexOf(maxDB)
                     val pitch = VocalRange(maxIndex * resolution)
-                    sizeView.text = (maxIndex * resolution).toString() + "Hz  " + pitch.getOctaveName()
-                    sizeView.setTextColor(pitch.getPitchColor())
+                    sizeView?.text = (maxIndex * resolution).toString() + "Hz  " + pitch.getOctaveName()
+                    sizeView?.setTextColor(pitch.getPitchColor())
                 }
             }
             finally{
